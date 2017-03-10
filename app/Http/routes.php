25494b509a9dd91c905516318fea027c11e0e5b5
Evi Lolita Apriyani <evilolita27@gmail.com>
2017-03-10 10:15:05 +0800

    Posttest 2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,13 @@ Route::get('/', function () {
 Route::get('/public', function () {
     return ('Nama Saya : Evi Lolita Apriyani');
 });
+Route::get('hello-world', function () {
+    return ('hello world');
+});
+
+Route::get('pengguna/{pengguna}', function ($pengguna) {
+    return "hello world dari pengguna $pengguna";//route parameter
+});
+Route::get('berita/{berita?}', function ($berita = "Laravel 5") {
+    return "berita $berita belum dibaca";//route parameter
+});
